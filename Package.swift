aba2907bc076d4aa4e4a0d53659267a2c37bc6b2
Nguyen Hai Dang <nhdang103@gmail.com>
2026-09-21 12:41:35 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "SwiftDate",
     defaultLocalization: "it",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v15), .watchOS(.v6), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
